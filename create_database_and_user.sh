4168cbc6b2c0a700bcbe5cac6fdff5632527b752
Tellardis <gomylist20@gmail.com>
2018-08-09 15:56:40 +0300

    script

---
+++ create_database_and_user.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+
+mysql -uroot -proot <<MYSQL_SCRIPT
+CREATE DATABASE $1;
+CREATE USER '$1'@'localhost' IDENTIFIED BY '$2';
+GRANT ALL PRIVILEGES ON $1.* TO '$1'@'localhost';
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
+
+echo "MySQL user created."
+echo "Username:   $1"
+echo "Password:   $2"
